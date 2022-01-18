bc9b63013cebd5f8525df45d61e45dfc43e78471
Me <pewpewololobro@gmail.com>
2022-01-18 14:13:08 +0700

    Module bot

---
+++ bot.py
@@ -0,0 +1,10 @@
+from aiogram import Bot, types, Dispatcher
+
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot, storage=storage)
+
+async def on_startup(_):
+	print('Bot started')
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
