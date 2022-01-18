43a5d03ddc47e490633a0108573c16fac0aeec3b
Me <pewpewololobro@gmail.com>
2022-01-18 16:18:21 +0700

    Get Test

--- server.py
+++ server.py
@@ -2,8 +2,9 @@ from bot import bot, dp
 from aiogram.utils import executor
 from aiogram import types
 from model import User, Word, Translate, WordTranslate
-import re
+import re, json
 import peewee
+from random import shuffle
 
 pattern = re.compile(r'/add[\s]([\w]+)[\s]([\w]+)')
 
@@ -23,13 +24,31 @@ async def add_word(message : types.Message):
 	await message.answer(f'Добавлео слово "{raw_word}" с переводом "{raw_translate}"')
 
 @dp.message_handler(commands = ['test'])
-async def get_test(message, user = None):
+async def get_test(message : types.Message, user = None):
 	user = user or User.get(external_id = message.from_user.id, chat_id = message.chat.id)
 	pairs = WordTranslate.select()\
 	.where(WordTranslate.user == user)\
-	.order_by(peewee.fn.Radom())\
+	.order_by(peewee.fn.Random())\
 	.limit(4)
-	
+	markup = types.InlineKeyboardMarkup()
+	buttons = []
+	question = None
+	for row in pairs:
+		word = Word.get(Word.id == row.word.id, Word.user == row.user)
+		translate = Translate.get(Translate.id == row.translate.id, Translate.user == row.user)
+		if question is None:
+			question = word
+		btn = types.InlineKeyboardButton(
+			text = f'{row.translate.translate}',
+			callback_data = json.dumps(
+				{'t' : 'a', 'q' : question.id, 'a' : translate.id}
+			)
+		)
+		buttons.append(btn)
+	shuffle(buttons)
+	markup.add(*buttons[:2])
+	markup.add(*buttons[2:])
+	await bot.send_message(user.chat_id, f"Слово {question.word}", reply_markup = markup)
 
 if __name__ == "__main__":
 	executor.start_polling(dp, skip_updates=True)
